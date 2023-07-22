33b9b62a900fd2c5f41404c0f8adad72d3ffc238
Braian-Quintian <bquintianromero@gmail.com>
2023-07-22 18:30:32 -0500

    feat: :sparkles: Se crea el redireccionamiento a la carpeta de manejo de errores

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
-
+import { handleInternalServerError } from '../src/errors/errors.js';
 const app = express();
 app.use(express.json());
 
 app.use((err, res) => {
     handleInternalServerError(err, res);
 });
-export default app;
+export default app; 
