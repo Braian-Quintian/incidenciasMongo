e64af7ef1fe300626bb21656c8889c9ae3381eda
Braian-Quintian <bquintianromero@gmail.com>
2023-07-23 03:04:33 -0500

    feat: :sparkles: Se añade el enpoint para hacer un post a la tabla trainer

--- src/middlewares/trainers.js
+++ src/middlewares/trainers.js
@@ -3,6 +3,7 @@ import { methodsTrainers } from '../routes/trainers.routes.js';
 const router = Router();
 
 router.get('/',methodsTrainers.getTrainers);
+router.post('/',methodsTrainers.addTrainer);
 
 export {
     router
