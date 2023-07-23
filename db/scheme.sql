f49cba12273aa343beeebdd0f26c33f9af3a8dd5
Braian-Quintian <bquintianromero@gmail.com>
2023-07-22 19:04:37 -0500

    feat: :sparkles: Se crea las tablas de  los tipos de areas

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,3 +1,8 @@
 CREATE DATABASE db_incidenciasTecnicas;
 
 USE db_incidenciasTecnicas;
+
+CREATE TABLE TipoArea(
+    id_tipo_area INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(25) NOT NULL UNIQUE
+);
