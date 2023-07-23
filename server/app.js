f27a4d720f4367aed908c6f46463f58c3dddc728
Braian-Quintian <bquintianromero@gmail.com>
2023-07-22 23:28:24 -0500

    feat: :sparkles: Se agrega el endpoint para hacer un GET de la tabla trainers

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { methodsTrainers } from '../src/routes/trainers.routes.js';
 import { handleInternalServerError } from '../src/errors/errors.js';
 const app = express();
 app.use(express.json());
 
+app.use('/trainers',methodsTrainers.getTrainers);
+
 app.use((err, res) => {
     handleInternalServerError(err, res);
 });
-export default app; 
+export default app;
