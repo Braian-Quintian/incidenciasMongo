7edb0d525ba6aaa4dbf162fc9440a60ecd218042
Braian-Quintian <bquintianromero@gmail.com>
2023-07-22 23:29:49 -0500

    feat: :sparkles: Se agrega la exportación de handleInternalServerError

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -3,3 +3,7 @@ function handleInternalServerError(err, res) {
     console.error(err);
     res.status(500).json({ message: 'Internal Server Error. Please try again later.' });
 }
+
+export {
+    handleInternalServerError
+}
