3dc4b844c0fb999b4a60d2007073866cdac83e56
Braian-Quintian <bquintianromero@gmail.com>
2023-07-22 19:06:03 -0500

    feat: :sparkles: Se crea el insert into para ara de campus

--- db/scheme.sql
+++ db/scheme.sql
@@ -17,3 +17,11 @@ CREATE TABLE AreaCampus(
     tipo_area INT NOT NULL,
     FOREIGN KEY (tipo_area) REFERENCES TipoArea(id_tipo_area)
 );
+
+INSERT INTO AreaCampus (nombre, tipo_area) VALUES
+('Apolo', 1),
+('Artemis',1),
+('Sputnik',1),
+('Skylab',1),
+('Corvus', 2),
+('Endor',2);
