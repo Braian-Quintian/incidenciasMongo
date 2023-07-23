b935ef9ad8aae2b26347b0b373db53a4975fb6aa
Braian-Quintian <bquintianromero@gmail.com>
2023-07-23 03:58:49 -0500

    feat: :sparkles: Se crea el archivo que genera el token

---
+++ src/middlewares/autorizacion.js
@@ -0,0 +1,23 @@
+import { SignJWT } from 'jose';
+import conexion from "../connection/credentials.js";
+
+export async function generateToken(id, nombre) {
+  let json = {
+    id: id,
+    nombre: nombre,
+  };
+
+  try {
+    const encoder = new TextEncoder();
+    const jwtconstructor = new SignJWT({ json });
+    const jwt = await jwtconstructor
+      .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+      .setIssuedAt()
+      .setExpirationTime("30s")
+      .sign(encoder.encode(conexion.token));
+
+    return jwt;
+  } catch (error) {
+    throw error;
+  }
+}
