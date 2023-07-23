4fe4fb7ec20174a702c1106cfc88f66ab2b9c56b
Braian-Quintian <bquintianromero@gmail.com>
2023-07-23 03:03:59 -0500

    feat: :sparkles: Se agregan dos manejos de errores más

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -3,7 +3,25 @@ function handleInternalServerError(err, res) {
     console.error(err);
     res.status(500).json({ message: 'Internal Server Error. Please try again later.' });
 }
+// Función para manejar errores de entrada duplicada (Error 400)
+function handleDuplicateEntryError(res) {
+    res.status(400).json({ message: 'Error creating bodega. The bodega name already exists. Please provide a unique name for the bodega.' });
+}
+
+// Función para manejar errores de datos inválidos (Error 400)
+function handleInvalidDataError(res, message) {
+    const errorMessage = 'Error de datos inválidos: ' + message; // Agrega un prefijo al mensaje de error
+    res.status(400).json({ message: errorMessage });
+}
+
+// Función para manejar errores de bodega no existente (Error 400)
+function handleNoExist(res) {
+    res.status(400).json({ message: 'Error creating bodega. The user to join does not exist.' });
+}
 
 export {
-    handleInternalServerError
+    handleInternalServerError,
+    handleDuplicateEntryError,
+    handleInvalidDataError,
+    handleNoExist
 }
