0f51d16d647c768d7e8832a53ad4b67e9e6a2730
Braian-Quintian <bquintianromero@gmail.com>
2023-07-22 21:29:47 -0500

    style: :art: Formato al código

--- db/scheme.sql
+++ db/scheme.sql
@@ -7,21 +7,9 @@ CREATE TABLE TipoArea(
     nombre VARCHAR(25) NOT NULL UNIQUE
 );
 
-INSERT INTO TipoArea(nombre) VALUES
-('Area Training'),
-('Area Review');
-
 CREATE TABLE AreaCampus(
     id_area INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(25) NOT NULL UNIQUE,
     tipo_area INT NOT NULL,
     FOREIGN KEY (tipo_area) REFERENCES TipoArea(id_tipo_area)
 );
-
-INSERT INTO AreaCampus (nombre, tipo_area) VALUES
-('Apolo', 1),
-('Artemis',1),
-('Sputnik',1),
-('Skylab',1),
-('Corvus', 2),
-('Endor',2);
