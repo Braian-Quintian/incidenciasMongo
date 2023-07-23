5e7581570396ec49c097d995c8c9d6ccb1d4f59f
Braian-Quintian <bquintianromero@gmail.com>
2023-07-23 03:58:07 -0500

    feat: :sparkles: Se añade el middleware del token

--- server/app.js
+++ server/app.js
@@ -1,13 +1,29 @@
 import express from 'express';
-import { router as Trainers} from '../src/middlewares/trainers.js';
+import { router as Trainers } from '../src/middlewares/trainers.js';
+import { generateToken } from '../src/middlewares/autorizacion.js';
+import { verifyToken } from '../src/middlewares/verifyToken.js';
+
 import { handleInternalServerError } from '../src/errors/errors.js';
+
 const app = express();
 app.use(express.json());
 
+app.get('/autorizacion/:id/:nombre', async (req, res) => {
+  const id = req.params.id;
+  const nombre = req.params.nombre;
 
-app.use('/trainers',Trainers);
+  try {
+    const jwt = await generateToken(id, nombre);
+    res.send({ jwt });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.use('/trainers', verifyToken, Trainers);
 
 app.use((err, res) => {
-    handleInternalServerError(err, res);
+  handleInternalServerError(err, res);
 });
+
 export default app;
