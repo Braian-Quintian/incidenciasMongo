75cca8db68b17182f6fe99abff5e64deca3eb3e4
Braian-Quintian <bquintianromero@gmail.com>
2023-07-24 13:30:54 -0500

    feat: :sparkles: Se modulariza la peticion del token

--- server/app.js
+++ server/app.js
@@ -2,23 +2,13 @@ import express from 'express';
 import { router as Trainers } from '../src/middlewares/trainers.js';
 import { router as Equipo } from '../src/middlewares/equipo.js';
 import { router as Incidencias } from '../src/middlewares/incidencias.js';
-import { generateToken } from '../src/keys/autorizacion.js';
-import { verifyToken } from '../src/keys/verifyToken.js';
+import { autenticacion } from '../src/security/autenticacion.js';
+import { verifyToken } from '../src/security/keys/verifyToken.js';
 import { handleInternalServerError } from '../src/errors/errors.js';
 const app = express();
 app.use(express.json());
 
-app.get('/autorizacion/:id/:nombre', async (req, res) => {
-  const id = req.params.id;
-  const nombre = req.params.nombre;
-
-  try {
-    const jwt = await generateToken(id, nombre);
-    res.send({ jwt });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+app.get('/autorizacion/:id/:nombre', autenticacion);
 
 app.use('/trainers', verifyToken, Trainers);
 app.use('/equipos', verifyToken, Equipo);
