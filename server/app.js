a77691a6f84c93c314e8831fe8f5e312660b9e07
Braian-Quintian <bquintianromero@gmail.com>
2023-08-23 13:42:26 -0500

    feat: :construction: Se sigue trabajando

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import {login} from '../src/security/keys/login.js';
 const app = express();
 app.use(express.json());
-app.get('/login', autenticacion);
-app.use('/:collection', dynamicRouter);
-app.use((err, req, res, next) => {handleInternalServerError(err, res);});
+app.use('/login', login);
+// app.use('/:collection', dynamicRouter);
+// app.use((err, req, res, next) => {handleInternalServerError(err, res);});
 export default app;
