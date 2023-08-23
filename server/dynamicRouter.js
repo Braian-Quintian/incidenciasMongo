7e2c4aa3c5bf1900a4719f99930487673c5e40c5
Braian-Quintian <bquintianromero@gmail.com>
2023-08-23 17:22:06 -0500

    feat: :construction: Se sigue trabajando

--- server/dynamicRouter.js
+++ server/dynamicRouter.js
@@ -1,29 +1,16 @@
-import { router as Usuario } from '../src/middleware/usuario.js';
-import { router as Citas } from '../src/middleware/citas.js';
-import { router as Medicos } from '../src/middleware/medicos.js';
-
+import { router as Equipo } from '../src/middleware/equipo.js';
+// import { router as Incidencias } from '../src/middleware/incidencias.js';
+// import { router as Trainers } from '../src/middleware/trainers.js';
 export async function dynamicRouter(req, res, next) {
     const { collection } = req.params;
-    const user = req.user;
-    if (user) {
-        if (user.rol === 'sudosu') {
-            switch (collection) {
-                case 'usuarios':
-                    return Usuario(req, res, next);
-                case 'citas':
-                    return Citas(req, res, next);
-                case 'medicos':
-                    return Medicos(req, res, next);
-                default:
-                    return res.status(404).send({ error: 'Ruta no encontrada' });
-            }
-        } else if (user.rol === 'empleado') {
-            // if (collection === 'bodegas') {
-            //     return Bodegas(req, res, next);} 
-            // else {
-                return res.status(401).json({ error: 'Unauthorized' });
-            // }
-        }
+    switch (collection) {
+        case 'equipos':
+            return Equipo(req, res, next);
+        case 'incidencias':
+            return Incidencias(req, res, next);
+        case 'trainers':
+            return Trainers(req, res, next);
+        default:
+            return res.status(404).send({ error: 'Ruta no encontrada' });
     }
-    res.status(401).json({ error: 'Unauthorized' });
 };
