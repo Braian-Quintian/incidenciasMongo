da5a5622b9acac18ca66e06dd35915b0d3b52894
Braian-Quintian <bquintianromero@gmail.com>
2023-08-23 17:22:33 -0500

    feat: :construction: Se sigue trabajando en el validar token

--- src/config/JWT.js
+++ src/config/JWT.js
@@ -6,7 +6,8 @@ const db = await connect();
 
 const createToken = async (req, res, next) => {
     if (Object.keys(req.body).length === 0) return res.status(400).send({mesaage: "Datos no enviados"});
-    const result = await db.collection("Trainer").findOne({trainer_emailP: req.body.email});
+    const result = await db.collection("Trainer").findOne({trainer_cc: parseInt(req.body.cc)});
+    console.log(result);
     if (!result) return res.status(401).send({message: "Usuario no encontrado"});
     const encoder = new TextEncoder();
     const id = result._id.toString();
@@ -19,6 +20,31 @@ const createToken = async (req, res, next) => {
     next();
 }
 
+const validarToken = async (req, token) => {
+    try {
+        const encoder = new TextEncoder();
+        const jwtData = await jwtVerify(token,encoder.encode(conexion.token)
+        );
+        // console.log(req.baseUrl)
+        let res = await db.collection('Trainer').findOne({_id: new ObjectId(jwtData.payload.id),[`trainer_permisos.${req.baseUrl}`]: { $exists: true }});
+
+        if (res.trainer_permisos[`${req.baseUrl}`][0] === '*') {
+            console.log('SI TIENE PERMISOS');
+        } else {
+            let au = await db.collection('Trainer').findOne({_id: new ObjectId(jwtData.payload.id),[`trainer_permisos.${req.baseUrl}`]: { $exists: true, $in: [`${req.headers["accept-version"]}`] }});
+            console.log(au);
+        }
+
+        // , $in: [`${req.headers["accept-version"]}`] 
+        console.log(res)
+        let {_id, permisos, ...usuario} = res;
+        return usuario;
+    } catch (error) {
+        return false;
+    }
+}
+
 export {
-    createToken
+    createToken,
+    validarToken
 }
