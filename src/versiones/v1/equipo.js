faed3dd58ce47b870a69d768444e594b79162364
Braian-Quintian <bquintianromero@gmail.com>
2023-08-24 16:54:15 -0500

    feat: :construction: Se está trabajando en el primer GET

--- src/versiones/v1/equipo.js
+++ src/versiones/v1/equipo.js
@@ -1,5 +1,20 @@
-export const equipoV1 = (req, res, next) => {
+import { validationResult } from "express-validator";
+import { connect } from '../../connection/connection.js'
+const db = await connect();
+
+export const equipoV1 = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    let { nombre:nom} = req.body;
+    let data = Object.assign({nom})
+    console.log(data);
     if(!req.rateLimit) return; 
-    console.log('ENTRAMO MI GENTE')
-    // res.status(req.data.status).send(req.data);
+    try{
+        const result = await db.collection('Equipo').find().toArray();
+        res.status(200).json(result);
+    }catch(error) {
+        res.status(500).json({ message: error.message });
+    }
 }
