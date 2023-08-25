cebd65da1b2c94dc801e2b926d2b54eba934957d
Braian-Quintian <bquintianromero@gmail.com>
2023-08-25 13:54:07 -0500

    fix: :bug: Se corrige el mal uso del limit que impedia el acceso

--- src/middleware/equipo.js
+++ src/middleware/equipo.js
@@ -1,10 +1,9 @@
 import Routes from 'express';
-import { limitLogin } from "../config/limit.js";
 import passportHelper from '../config/passportHelpert.js';
 import equiposRoutes from '../routes/equipos.routes.js'; // Importa el enrutador de rutas
 const router = Routes();
 
-router.use(limitLogin(), passportHelper.authenticate('bearer', { session: false }));
+router.use(passportHelper.authenticate('bearer', { session: false }));
 router.use('/', equiposRoutes); // Utiliza el enrutador de rutas
 
 export {
