226302efadebd9090e729e36326bb4569c1d84b9
Braian-Quintian <bquintianromero@gmail.com>
2023-08-25 13:54:32 -0500

    feat: :sparkles: Se añade el enrutamiento para el post

--- src/routes/equipos.routes.js
+++ src/routes/equipos.routes.js
@@ -1,7 +1,7 @@
 import routesVersioning from 'express-routes-versioning';
 import { Router } from 'express';
 import { limitEquiposG } from '../config/limit.js';
-import { equipoV1 } from '../versiones/v1/equipo.js';
+import { equipoV1, equipoV1_1 } from '../versiones/v1/equipo.js';
 
 const equiposRouter = Router();
 const version = routesVersioning();
@@ -11,7 +11,7 @@ equiposRouter.get('/',limitEquiposG(), version({
 }));
 
 equiposRouter.post('/',limitEquiposG(), version({
-    "1.0.1": equipoV1
+    "1.0.0": equipoV1_1
 }));
 
 export default equiposRouter;
