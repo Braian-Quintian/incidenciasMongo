80875a3e7512b46bbfd538429806c1c2c083f27e
Braian-Quintian <bquintianromero@gmail.com>
2023-08-25 13:29:49 -0500

    perf: :sparkles: Se mejora el rendimiento de la consulta iterando una sola vez

--- src/versiones/v1/equipo.js
+++ src/versiones/v1/equipo.js
@@ -1,18 +1,26 @@
-import { validationResult } from "express-validator";
 import { connect } from '../../connection/connection.js'
 const db = await connect();
 
 export const equipoV1 = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    let { nombre:nom} = req.body;
-    let data = Object.assign({nom})
-    console.log(data);
     if(!req.rateLimit) return; 
     try{
-        const result = await db.collection('Equipo').find().toArray();
+        const result = await db.collection('Equipo').aggregate([
+            {
+              $project: {
+                _id: 0,
+                "id-equipo": "$equipo_id",
+                "nombre-equipo": "$equipo_nombre",
+                "tipo-equipo": "$equipo_tipo",
+                "marca-equipo": "$equipo_marca",
+                "modelo-equipo":"$equipo_modelo",
+                "serie-equipo": "$equipo_serie",
+                "estado-equipo":"$equipo_estado",
+                "area-equipo": "$equipo_area",
+                "creacion-equipo": "$equipo_creacion",
+                "modificacion-equipo": "$equipo_modificacion" 
+              }
+            },
+          ]).toArray();
         res.status(200).json(result);
     }catch(error) {
         res.status(500).json({ message: error.message });
