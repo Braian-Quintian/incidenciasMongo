1bd6c9ad1b216a2e1c040f2233503343202fa164
Braian-Quintian <bquintianromero@gmail.com>
2023-08-25 13:30:31 -0500

    refactor: :construction: Se está trabajando en esta validacion

--- src/dist/equipo.js
+++ src/dist/equipo.js
@@ -1,19 +1,18 @@
-import {check, body} from 'express-validator'
-let entidad = {
-    entidad1: "nombre",
-    entidad2: "descripcion",
-}
+import { body } from 'express-validator';
 
-let {entidad1:nombre,entidad2:descripcion} = entidad;
+export const validateEquipo = (equipo) => {
+    console.log(equipo);
+    const { nombre, descripcion } = equipo;
 
-export const equiposDTO = [
-    body(`${nombre}`).notEmpty().withMessage(`El ${nombre} del equipo es requerido`)
-    .isString().withMessage(`El dato tiene que ser un string`)
-    .isLength({min: 5}).withMessage(`El ${nombre} tiene que tener minimo 5 caracteres`)
-    .matches(/^[a-zA-Z]+$/).withMessage(`El ${nombre} solo puede contener letras`),
+    return [
+        body(nombre).notEmpty().withMessage(`El ${nombre} del equipo es requerido`)
+            .isString().withMessage(`El dato tiene que ser un string`)
+            .isLength({ min: 5 }).withMessage(`El ${nombre} tiene que tener mínimo 5 caracteres`)
+            .matches(/^[a-zA-Z]+$/).withMessage(`El ${nombre} solo puede contener letras`),
 
-    body(`${descripcion}`).notEmpty().withMessage(`La ${descripcion} del equipo es requerida`)
-    .isString().withMessage(`El dato tiene que ser un string`)
-    .isLength({min: 10}).withMessage(`El ${nombre} tiene que tener minimo 10 caracteres`)
-    .matches(/^[a-zA-Z]+$/).withMessage(`El ${nombre} solo puede contener letras`)
-]
+        body(descripcion).notEmpty().withMessage(`La ${descripcion} del equipo es requerida`)
+            .isString().withMessage(`El dato tiene que ser un string`)
+            .isLength({ min: 10 }).withMessage(`La ${descripcion} tiene que tener mínimo 10 caracteres`)
+            .matches(/^[a-zA-Z]+$/).withMessage(`La ${descripcion} solo puede contener letras`)
+    ];
+};
